a0030be55e90d175a231706b35e8858a8a65afd6
Asitha Navaratne <asithan476@gmail.com>
2021-11-10 22:55:19 +0530

    Justified comments

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int main(void){
 	
 	while(1){
 		result = ADC_CONVERSION();
-		OCR1A = 1000 + result*4.2228739;				///< Convert ADC result to a PWM signal corresponding to an angle.
+		OCR1A = 1000 + result*4.2228739;			///< Convert ADC result to a PWM signal corresponding to an angle.
 	}
 }
 
@@ -24,7 +24,7 @@ int main(void){
  */
 
 void PORT_INIT(void){
-	DDRD |= (1<<PD5);									///< Set OC1A as output to generate PWM signal.
+	DDRD |= (1<<PD5);						///< Set OC1A as output to generate PWM signal.
 }
 
 /*!
@@ -32,8 +32,8 @@ void PORT_INIT(void){
  */
 
 void ADC_INIT(void){
-	ADMUX = (1<<REFS0);								///< Set ADC reference to AVCC and analog input mode to single-ended input of ADC0.
-	ADCSRA = ((1<<ADEN)|(1<<ADPS2)|(1<<ADPS1));		///< Enable ADC and set ADC prescalar to 64.
+	ADMUX = (1<<REFS0);						///< Set ADC reference to AVCC and analog input mode to single-ended input of ADC0.
+	ADCSRA = ((1<<ADEN)|(1<<ADPS2)|(1<<ADPS1));			///< Enable ADC and set ADC prescalar to 64.
 }
 
 /*!
@@ -41,9 +41,9 @@ void ADC_INIT(void){
  */
 
 void TIMER_INIT(void){
-	TCCR1A = (1<<COM1A1)|(1<<WGM11);					///< Select Fast PWM mode with ICR1 as TOP. Clear OC1A on compare match and Set OC1A at BOTTOM (non-inverting mode).
+	TCCR1A = (1<<COM1A1)|(1<<WGM11);				///< Select Fast PWM mode with ICR1 as TOP. Clear OC1A on compare match and Set OC1A at BOTTOM (non-inverting mode).
 	TCCR1B = (1<<WGM12)|(1<<WGM13)|(1<<CS11);			///< Select Fast PWM mode with ICR1 as TOP and set prescalar value of 8.
-	ICR1 = 19999;										///< Set TOP value to determine frequency of the PWM signal.
+	ICR1 = 19999;							///< Set TOP value to determine frequency of the PWM signal.
 }
 
 /*!
@@ -52,7 +52,7 @@ void TIMER_INIT(void){
  */
 
 uint16_t ADC_CONVERSION(void){
-	ADCSRA |= (1<<ADSC);								///< Start ADC conversion.
-	while(ADCSRA & (1<<ADSC));							///< Wait until conversion is complete.
-	return ADC;											///< Return result of conversion.
+	ADCSRA |= (1<<ADSC);						///< Start ADC conversion.
+	while(ADCSRA & (1<<ADSC));					///< Wait until conversion is complete.
+	return ADC;							///< Return result of conversion.
 }
